f0c5119bf34270cc1b0014a141180f34ed9926fc
gyongcode <gyongcode@gmail.com>
2025-08-05 17:21:01 +0900

    fix: mainpage 파일 확장자 jsx로 변경

--- src/app/components/MainPage.jsx
+++ src/app/components/MainPage.jsx
@@ -1,14 +1,14 @@
 "use client";
 
 import styles from "../styles/MainPage.module.css";
-import Header from "./Header";
-import HeroIntro from "./HeroIntro";
-import HeroSection from "./HeroSection";
-import FeatureCards from "./FeatureCards";
-import CampaignSection from "./CampaignSection";
-import InfluencerSection from "./InfluencerSection";
-import CTASection from "./CTASection";
-import Footer from "./Footer";
+import Header from "./Header.jsx";
+import HeroIntro from "./HeroIntro.jsx";
+import HeroSection from "./HeroSection.jsx";
+import FeatureCards from "./FeatureCards.jsx";
+import CampaignSection from "./CampaignSection.jsx";
+import InfluencerSection from "./InfluencerSection.jsx";
+import CTASection from "./CTASection.jsx";
+import Footer from "./Footer.jsx";
 
 export default function MainPage() {
   return (
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,4 @@
-import MainPage from "./components/MainPage";
+import MainPage from "./components/MainPage.jsx";
 
 export default function Home() {
   return (
