386551c7899b08e0e4ffa519dc7815d05917be05
gyongcode <gyongcode@gmail.com>
2025-08-05 17:20:37 +0900

    fix: header 수정

--- src/app/sns/layout.js
+++ src/app/sns/layout.js
@@ -7,26 +7,6 @@ import { SnsProvider } from "./context/SnsContext";
 export default function SnsLayout({ children }) {
   return (
     <SnsProvider>
-      <Head>
-        <title>SNS 관리 - PetFul</title>
-        <meta
-          name="description"
-          content="반려동물 SNS 계정 관리 및 분석 대시보드"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Pretendard:wght@300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
       {/* Header */}
       <Header />
       <TabNavigation />
