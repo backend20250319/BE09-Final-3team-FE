f8648d8a470b9cc13310f329739c19e32aabf742
LimHub <dla6963@naver.com>
2025-08-06 08:45:41 +0900

    feat : 알람 드롭다운 네비게이션바 구현 후 알람 아이콘이랑 연결

--- src/app/alarm/page.jsx
+++ src/app/alarm/page.jsx
@@ -1,8 +1,11 @@
 import AlarmPage from "../alarm/components/AlarmPage";
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
 
 
 export default function Home() {
     return (
+
         <div className="min-h-screen">
             <AlarmPage />
         </div>
