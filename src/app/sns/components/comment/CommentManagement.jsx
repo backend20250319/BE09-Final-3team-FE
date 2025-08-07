81354afc255556afb5a223ab735de774282bb4a0
gyongcode <gyongcode@gmail.com>
2025-08-07 15:59:02 +0900

    fix: 댓글 분석 css 수정

--- src/app/sns/components/comment/CommentManagement.jsx
+++ src/app/sns/components/comment/CommentManagement.jsx
@@ -11,6 +11,9 @@ export default function CommentManagement() {
       {/* 댓글 분석 섹션 */}
       <CommentAnalytics />
 
+      {/* 섹션 간 간격 */}
+      <div style={{ marginBottom: "32px" }} />
+
       {/* 댓글 목록 섹션 */}
       <CommentList />
     </>
