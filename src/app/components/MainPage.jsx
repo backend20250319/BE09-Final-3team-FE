660ff7794667dcc272d29b153a0d75230e8db941
NYoungLEE <skdud0694@gmail.com>
2025-08-11 10:56:51 +0900

    fix: petData 수정

--- src/app/components/MainPage.jsx
+++ src/app/components/MainPage.jsx
@@ -1,14 +1,12 @@
 "use client";
 
 import styles from "../styles/MainPage.module.css";
-import Header from "./Header.jsx";
 import HeroIntro from "./HeroIntro.jsx";
 import HeroSection from "./HeroSection.jsx";
 import FeatureCards from "./FeatureCards.jsx";
 import CampaignSection from "./CampaignSection.jsx";
 import InfluencerSection from "./InfluencerSection.jsx";
 import CTASection from "./CTASection.jsx";
-import Footer from "./Footer.jsx";
 
 export default function MainPage() {
   return (
